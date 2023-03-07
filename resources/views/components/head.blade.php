aa88c6d082ee08f1c4e7f928cd17c433dafe1bc3
issdilshod <dilshod@themembers.com>
2023-03-07 10:02:55 +0300

    mf

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -1,9 +1,9 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">   
-<meta name="description" content="<?php if (isset($seo)){ echo $seo->description; } ?>"> 
+<meta name="description" content="<?php if (isset($seo)){ echo $seo['description']; } ?>"> 
 <link rel="canonical" href="{{url()->full()}}"/>
-<title>{{env('APP_NAME')}} | <?php if (isset($seo)){ echo $seo->title; } else if (isset($title)) { echo $title; } ?></title>     
+<title>{{env('APP_NAME')}} | <?php if (isset($seo)){ echo $seo['title']; } else if (isset($title)) { echo $title; } ?></title>     
 <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet">
 <link href="{{ asset('assets/css/swiper.min.css') }}" rel="stylesheet">
 <link href="{{ asset('assets/css/styles.css') }}" rel="stylesheet">  
